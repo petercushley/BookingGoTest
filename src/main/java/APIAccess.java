6ab8aeedb758a73ca51f39ab007d4e2ad14de3cd
Pedro95 <peter.cushley@gmail.com>
2018-09-03 11:21:37 +0100

    Test for accessing Dave API now passing

--- src/main/java/APIAccess.java
+++ src/main/java/APIAccess.java
@@ -1,6 +1,9 @@
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class APIAccess {
@@ -10,6 +13,12 @@ public class APIAccess {
     private WebTarget target = client.target(targetURI);
 
     public Response getTaxisFromSupplier(String pickup, String dropoff, String supplier) {
-        return null;
+        WebTarget taxiTarget = target.path(supplier + "/")
+                .queryParam("pickup", pickup)
+                .queryParam("dropoff", dropoff);
+
+        Invocation.Builder invocationBuilder = taxiTarget.request();
+
+        return invocationBuilder.get();
     }
 }
